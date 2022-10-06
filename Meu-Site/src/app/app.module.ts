952567b57e86622e03472c219b2404fe419ceb8a
Luis Henrique da Silva Carvalho <luisdev9225@gmail.com>
2022-10-06 00:16:58 -0300

    :sparkles: feat: Criação do menu de navegação

--- Meu-Site/src/app/app.module.ts
+++ Meu-Site/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+//pages
+import { HeaderComponent } from './components/pages/header/header.component';
+import { CurriculoComponent } from './components/pages/header/curriculo/curriculo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CurriculoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
